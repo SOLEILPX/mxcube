9a855b7c932cab1a0d439755c27554b04aedbd05
Martin Savko <savko@synchrotron-soleil.fr>
2014-06-19 11:34:18 +0200

    Resizing the buttons, adding apertureAlign button

--- Bricks/SOLEIL/SoleilHutchMenuBrick.py
+++ Bricks/SOLEIL/SoleilHutchMenuBrick.py
@@ -25,18 +25,17 @@ class SoleilHutchMenuBrick(HutchMenuBrick.HutchMenuBrick):
         self.sampleCentreBox.setSizePolicy(QSizePolicy.Fixed,QSizePolicy.Fixed)
         self.buttonsBox.setSizePolicy(QSizePolicy.Fixed,QSizePolicy.Fixed)
 
-
         self.layout().addWidget(self.sampleCentreBox)
 
         self.buttonCentre=HutchMenuBrick.MenuButton(self.buttonsBox,"Centre")
-        self.buttonCentre.setMinimumSize(QSize(75,50))
+        self.buttonCentre.setMinimumSize(QSize(50,40))
         self.connect(self.buttonCentre,PYSIGNAL('executeCommand'),self.centreSampleClicked)
         self.connect(self.buttonCentre,PYSIGNAL('cancelCommand'),self.cancelCentringClicked)
 
         self.buttonAccept = QToolButton(self.buttonsBox)
         self.buttonAccept.setUsesTextLabel(True)
         self.buttonAccept.setTextLabel("Save")
-        self.buttonAccept.setMinimumSize(QSize(75,50))
+        self.buttonAccept.setMinimumSize(QSize(50,40))
         self.buttonAccept.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
         self.buttonAccept.setEnabled(False)
         QObject.connect(self.buttonAccept,SIGNAL('clicked()'),self.acceptClicked)
@@ -45,7 +44,7 @@ class SoleilHutchMenuBrick(HutchMenuBrick.HutchMenuBrick):
         self.buttonReject = QToolButton(self.buttonsBox)
         self.buttonReject.setUsesTextLabel(True)
         self.buttonReject.setTextLabel("Reject")
-        self.buttonReject.setMinimumSize(QSize(75,50))
+        self.buttonReject.setMinimumSize(QSize(50,40))
         self.buttonReject.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
         self.buttonReject.setEnabled(False)
         self.buttonReject.hide()
@@ -57,19 +56,26 @@ class SoleilHutchMenuBrick(HutchMenuBrick.HutchMenuBrick):
         self.buttonSnapshot = QToolButton(self.sampleCentreBox)
         self.buttonSnapshot.setUsesTextLabel(True)
         self.buttonSnapshot.setTextLabel("Snapshot")
-        self.buttonSnapshot.setMinimumSize(QSize(75,50))
+        self.buttonSnapshot.setMinimumSize(QSize(50,40))
         self.buttonSnapshot.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
         QObject.connect(self.buttonSnapshot,SIGNAL('clicked()'),self.saveSnapshot)
 
         self.buttonBeamPosition = QToolButton(self.sampleCentreBox)
         self.buttonBeamPosition.setUsesTextLabel(True)
-        self.buttonBeamPosition.setTextLabel("CheckBeam")
-        self.buttonBeamPosition.setMinimumSize(QSize(75,50))
+        self.buttonBeamPosition.setTextLabel("BeamPosition")
+        self.buttonBeamPosition.setMinimumSize(QSize(50,40))
         self.buttonBeamPosition.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
         self.buttonBeamPosition.setPixmap(Icons.load("green_led"))
-
         QObject.connect(self.buttonBeamPosition, SIGNAL('clicked()'), self.beamPositionCheck)
-
+        
+        self.buttonApertureAlign = QToolButton(self.sampleCentreBox)
+        self.buttonApertureAlign.setUsesTextLabel(True)
+        self.buttonApertureAlign.setTextLabel("ApertureAlign")
+        self.buttonApertureAlign.setMinimumSize(QSize(50,40))
+        self.buttonApertureAlign.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+        self.buttonApertureAlign.setPixmap(Icons.load("Align"))
+        QObject.connect(self.buttonApertureAlign, SIGNAL('clicked()'), self.apertureAlign)
+        
     def beamPositionCheck(self):
         self.minidiff.beamPositionCheck()
         
